0610472061435da461dac134683990573dbe5e0c
nokika <nokika@protonmail.com>
2017-03-17 23:26:42 -0400

    Added interaction with options popup

--- contentscript.js
+++ contentscript.js
@@ -1,5 +1,27 @@
+// Variables used throughout program
+var muteChecked = false;
+var soundsChecked = false;
+var commandsChecked = false;
+
+// Initialize options variables
+function restore_options() {
+  chrome.storage.sync.get({
+    mute: true,
+    sounds: true,
+    commands: true
+  }, function(items) {
+    muteChecked = items.mute;
+    soundsChecked = items.sounds;
+    commandsChecked = items.commands;
+  });
+}
+
+// TODO: calling this right away, but is this function ever used again?
+restore_options();
+
 // Bunch of constants used throughout the program
 // TODO: organize these better into arrays or something maybe
+// TODO: rename this to something meaningful
 chatboxClass = "chat-lines";
 chatMessageClass1 = "chat-line";
 chatMessageClass2 = "chat-line highlight";
@@ -8,14 +30,56 @@ chatMessageClass4 = "chat-line admin";
 chatMessageContent = "message";
 chatMessageSenderClass = "data-sender";
 
-botCommands = ["!spin", "!heist"]
+// Arrays of messages to block
+botCommands = ["!brag", "!time", "!8ball", "!sounds"]
+
+// TODO: would a hashmap give better performance for these array?
+// similar space requirements? less cpu cycles?
+botSounds = [
+          // San Andreas Sounds
+          "!AssTech", "!DriveThru", "!Fries", "!Home", "!Input",
+	  "!LittleMan", "!OGLocRap", "!Porty", "!Shermhead", "!Smoke",
+	  "!Watching", "!Genius", "!4Life", "!Smoke2", "!Meth", "!Asshole",
+	  "!DemDuckets", "!Mamas", "!Suck", "!SendDuckets", "!MyDuckets",
+	  "!DamnTrain", "!NinjaStyle", "!RoadRage", "!CrackMan",
+
+           // Grand Theft Auto V
+	   "!TinyPeePee", "!Molester",
+
+	   // Gorillazrockstar
+	   "!GLying", "!GShootHerAss", "!GChatOrgy", "!HeatOfTheMoment",
+	   "!GorillazOrgy", "!GorillazBooty",
+
+	   // Popular Memes
+	   "!DeezNuts", "!Flavored", "!Fuck", "!JohnCena", "!JohnCena2",
+	   "!MudJug", "!Narrow", "!ShootHerAss", "!WhatAreThose", "!Lying",
+	   "!21", "!Jeff", "!Cry", "!Bruh", "!LeeroyJenkins", "!Lebron",
+	   "!Turtles", "!Whistle", "!Uhhh", "!Pussy", "!RKO", "!NotGay",
+	   "!JustDoIt", "!MVP", "!Laugh", "!DoItLive", "!OhJYeah",
+	   "!GetTheCamera", "!Haters", "!HellNaw", "!Damn", "!Butt",
+	   "!Hoodrat", "!DickButt", "!Neck", "!Rap", "!Profanity", "!Pear",
+	   "!BBQ", "!Legitness", "!MeatFist", "!Pull", "!Purse", "!Kitty",
+	   "!ThisSide", "!WTF", "!Triple", "!Milk", "!Dirty", "!TrainGuy",
+	   "!Crack", "!Grass", "!Candy", "!STFUNokika", "!Chip", "!ByTheWay",
+	   "!Bitch", "!Respek", "!Dammit", "!Cars", "!CashiePrank",
+
+	   // Personal Sounds
+	   "!Pow", "!ILL", "!PussyTown", "!NoTime", "!ScienceBitch", "!Whores",
+	   "!NokikasTruck", "!Cocksucker", "!RickRoll", "!Drama", "!Joke",
+	   "!DontListen", "!Pikachu", "!Rapist", "!HellYeah", "!FYou",
+           "!SuckMyBalls", "!High", "!JoeysWorldTour", "!Quiltie", "!Meep",
+	   "!WaySheGoes", "!PhoneSex", "!BigBoobs", "!ItsOnlyGame", "!OMG",
+           "!OhThru", "!FuckIlluminati", "!Truth", "!RapThru", "!SayItToMyFace",
+	   "!HiAdam", "!Bowling", "!FuckHugo", "!FuckedUp", "!RefuckIlluminati"]
 
 // Stores useful information about a chat message
 // Parameters:
 //   variable.text - message text
 //   variable.sender - name of message sender
 //   variable.type - type of message (highlight, action, admin, etc.)
-function Message (node) {
+//   TODO: I could parse the message content here (i.e. spin command, sound command...)
+//      could use this for better filtering later
+function Message(node) {
     // Get message text
     var messageSpan = node.getElementsByClassName(chatMessageContent)[0];
     this.text = messageSpan.innerHTML;
@@ -29,6 +93,20 @@ function Message (node) {
 
 
 
+// Simple helper function
+//   returns true if msg is in the list
+//   otherwise false
+function isMember(msg, list) {
+  for (i = 0; i < list.length; i++) {
+    if (msg.toLowerCase().includes(list[i])) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+
 
 /*****************************************************************************
 **                                                                          **
@@ -40,7 +118,7 @@ function Message (node) {
 // See if message matches any filters
 //   Return True if message should be deleted
 //   Return false if message has no match
-var filterMessage = function (msgObj) {
+var filterMessage = function(msgObj) {
     // TODO: match exceptions first (i.e. block user X unless they highlight me)
 
     // Simple example of allowing message on highlight
@@ -49,19 +127,29 @@ var filterMessage = function (msgObj) {
         return false;
     }
 
-    // TODO: match for more specific commands and messages
-    if (msgObj.sender.toLowerCase() == "hugo__bot") {
+    // if mute is checked block all
+    if (muteChecked == true) {
+      if (isMember(msgObj.text, botSounds) ||
+	  msgObj.sender.toLowerCase() == "hugo__bot" ||
+	  isMember(msgObj.text, botCommands)) {
         return true;
+      }
     }
 
-    // Example loop through bot commands
-    for (i = 0; i < botCommands.length; i++) {
-        if (msgObj.text.toLowerCase().includes(botCommands[i])) {
-	    // short circuit
+
+    // Should viewer sound commands be blocked?
+    if (soundsChecked) {
+      if (isMember(msgObj.text, botSounds)) {
+        return true;
+      }
+    }
+
+    // Should viewer commands be blocked?
+    if (commandsChecked) {
+        if (isMember(msgObj.text, botCommands)) {
 	    return true;
 	}
     }
-
 
     // Default return false, no matched filter
     return false;
@@ -139,4 +227,15 @@ var chatboxObserver = new MutationObserver(function (mutations, observer) {
 });
 
 // start the hooks
+
+// Start a hook listening for updates to options from the popup
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log("Received a message from updated options.");
+    muteChecked = request.mute;
+    soundsChecked = request.sounds;
+    commandsChecked = request.commands;
+  });
+
+// First find chat box once page is loaded, then find messages
 chatboxObserver.observe(htmlBody, config);
